efd3b9cf772d945761d73e933c9b81ed3b1b5984
Ionescu Mircea Mihail <mihail.mircea12@gmail.com>
2023-07-16 20:14:04 +0300

    deleted useless comments

--- printing.cpp
+++ printing.cpp
@@ -10,7 +10,6 @@
 
 extern LiquidCrystal e_lcd;
 
-//print bars
 void print_bars(){
   e_lcd.setCursor(7, 0);
   e_lcd.write(byte(INNER_PIPE_RIGHT));
@@ -21,7 +20,6 @@ void print_bars(){
   e_lcd.write(byte(INNER_PIPE_LEFT));
 }
 
-//print speed arrows
 void print_speed_arrows(){
   e_lcd.setCursor(0, 0);
   e_lcd.write(byte(RIGHT_ARROW));
@@ -30,7 +28,6 @@ void print_speed_arrows(){
   e_lcd.write(byte(LEFT_ARROW));
 }
 
-//delete speed arrows
 void delete_speed_arrows(){
   e_lcd.setCursor(0, 0);
   e_lcd.write(' ');
@@ -39,7 +36,6 @@ void delete_speed_arrows(){
   e_lcd.write(' ');
 }
 
-//print distance arrows
 void print_distance_arrows(){
   e_lcd.setCursor(9, 0);
   e_lcd.write(byte(RIGHT_ARROW));
@@ -48,7 +44,6 @@ void print_distance_arrows(){
   e_lcd.write(byte(LEFT_ARROW));
 }
 
-//delete distance arrows
 void delete_distance_arrows(){
   e_lcd.setCursor(9, 0);
   e_lcd.write(' ');
@@ -57,7 +52,6 @@ void delete_distance_arrows(){
   e_lcd.write(' ');
 }
 
-//print power arrows
 void print_power_arrows(){
   e_lcd.setCursor(0, 1);
   e_lcd.write(byte(RIGHT_ARROW));
@@ -66,7 +60,6 @@ void print_power_arrows(){
   e_lcd.write(byte(LEFT_ARROW));
 }
 
-//delete power arrows
 void delete_power_arrows(){
   e_lcd.setCursor(0, 1);
   e_lcd.write(' ');
@@ -75,7 +68,6 @@ void delete_power_arrows(){
   e_lcd.write(' ');
 }
 
-//print time arrows
 void print_time_arrows(){
   e_lcd.setCursor(9, 1);
   e_lcd.write(byte(RIGHT_ARROW));
@@ -84,7 +76,6 @@ void print_time_arrows(){
   e_lcd.write(byte(LEFT_ARROW));
 }
 
-//delete time arrows
 void delete_time_arrows(){
   e_lcd.setCursor(9, 1);
   e_lcd.write(' ');
@@ -93,26 +84,22 @@ void delete_time_arrows(){
   e_lcd.write(' ');
 }
 
-//print "speed"
 void print_word_speed(){
   e_lcd.setCursor(1, 0);
   e_lcd.print("speed");    
 }
 
-//print "dist"
 void print_word_dist(){
   e_lcd.setCursor(10, 0);
   e_lcd.print("dist");
   e_lcd.write(byte(KMS));
 }
 
-//print "power"
 void print_word_power(){
   e_lcd.setCursor(1, 1);
   e_lcd.print("power"); 
 }
 
-//print "time"
 void print_word_time(){
   e_lcd.setCursor(10, 1);
   e_lcd.print("time"); 
